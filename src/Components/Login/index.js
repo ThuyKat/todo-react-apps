1f9378de6208b225d2346958d48797adbd34bbdc
ThuyKat <katiefirst7@gmail.com>
2024-10-21 10:51:31 +1100

    login component added

---
+++ src/Components/Login/index.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import TodoList from "../TodoList";
+
+function Login(){
+    const [formInput,setFormInput] = useState({
+        username:'',
+        password:'',
+    });
+    const [userList,setUserList] = useState([{
+        username:'thuy',
+        password:'pass1'
+    },{
+        username:'kat',
+        password:'pass2'
+    }]);
+    const [hasMatchingUser,setHasMatchingUser] = useState(false);
+    function handleInputChange(event){
+        const{name,value} = event.target;
+        setFormInput({
+            ...formInput,
+            [name]:value,
+        }   
+        )
+    }
+    function handleOnSubmit(event){
+        event.preventDefault();
+        const match = userList.some(user=>user.username === formInput.username && user.password === formInput.password);
+        setHasMatchingUser(match);
+        if(!match){
+            alert('incorrect username or password')
+        }else{
+            setFormInput({
+                username:'',
+                password:''
+            })
+        }  
+    }
+    if(hasMatchingUser){
+        return <TodoList/> 
+    }
+    return(
+        <div>
+        
+        <form onSubmit={handleOnSubmit}>
+            <label htmlFor="username">Username: </label>
+            <input type="text" placeholder="Username" value={formInput.username} name="username" id="username" onChange={handleInputChange}/>
+            <label htmlFor="password">Password: </label>
+            <input type="password" placeholder="Password" value={formInput.password} name="password" id="password" onChange={handleInputChange}/>
+            <button type="submit">Submit</button>
+        </form>
+        
+    </div>
+    )
+}
+
+export default Login;
